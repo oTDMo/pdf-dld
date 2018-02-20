b8b5c9b34e578d31820aef3c4afc59197ba56f5e
Serban <serban.dobroiu@gmail.com>
2018-02-20 20:38:32 +0000

    pdf to json via base64 encoding then decoding

--- pdf_requests.py
+++ pdf_requests.py
@@ -1,14 +1,24 @@
 import requests
+import json
+from base64 import b64encode
 
-url = 'http://www.orimi.com/pdf-test.pdf'
+url = 'http://www.nimoz.pl/files/publications/4/loremipsum.pdf'
 
 r = requests.get (url)
 
-print(r.status_code)
+# print(r.status_code)
 # print(r.headers)
+# print(r.content)
+# print(base64.b64decode(r.text))
 
-file = open('test.pdf', "wb")
+b64_encoded = b64encode(r.content)
 
-file.write(r.content)
+string_encoded = b64_encoded.decode()
 
-file.close()
+data = {
+    'pdf_content': string_encoded
+}
+
+data = json.dumps(data)
+
+print(data)
