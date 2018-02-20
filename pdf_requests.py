d875a5a74172ac2a063c197a75ead6769bb33998
Serban <serban.dobroiu@gmail.com>
2018-02-20 20:56:06 +0000

    request to push the data into the db via api call

--- pdf_requests.py
+++ pdf_requests.py
@@ -2,9 +2,9 @@ import requests
 import json
 from base64 import b64encode
 
-url = 'http://www.nimoz.pl/files/publications/4/loremipsum.pdf'
+source_url = 'http://www.nimoz.pl/files/publications/4/loremipsum.pdf'
 
-r = requests.get (url)
+r_get = requests.get (source_url)
 
 err = "Something went wrong :("
 
@@ -13,12 +13,12 @@ err = "Something went wrong :("
 # print(r.content)
 # print(base64.b64decode(r.text))
 
-if r.status_code != 200:
+if r_get.status_code != 200:
     data = {
         'pdf_content': err
     }
 else:    
-    b64_encoded = b64encode(r.content)
+    b64_encoded = b64encode(r_get.content)
     
     string_encoded = b64_encoded.decode()
     
@@ -27,6 +27,15 @@ else:
     }
 
 data = json.dumps(data)
+
+target_url = ''
+
+headers = {
+    'Content': 'application/json'
+    # whatever else e.g. auth
+}
+
+r_post = requests.post (target_url, headers, json = data)
 
 # use data as payload to push pdf into database via another api call
 
