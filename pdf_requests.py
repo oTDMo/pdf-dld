1bafee0c3cabbd512520a1778a781b28f7d23b9d
Serban <serban.dobroiu@gmail.com>
2018-02-20 20:51:59 +0000

    added error branch

--- pdf_requests.py
+++ pdf_requests.py
@@ -6,18 +6,25 @@ url = 'http://www.nimoz.pl/files/publications/4/loremipsum.pdf'
 
 r = requests.get (url)
 
+err = "Something went wrong :("
+
 # print(r.status_code)
 # print(r.headers)
 # print(r.content)
 # print(base64.b64decode(r.text))
 
-b64_encoded = b64encode(r.content)
-
-string_encoded = b64_encoded.decode()
-
-data = {
-    'pdf_content': string_encoded
-}
+if r.status_code != 200:
+    data = {
+        'pdf_content': err
+    }
+else:    
+    b64_encoded = b64encode(r.content)
+    
+    string_encoded = b64_encoded.decode()
+    
+    data = {
+        'pdf_content': string_encoded
+    }
 
 data = json.dumps(data)
 
